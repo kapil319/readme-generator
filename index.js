8996dd30c474b972f0cfd0739e9beef076b8a3af
Jag Johal <jagjohal@Jags-MacBook-Air.local>
2023-10-12 21:05:29 +0100

    update to files

--- index.js
+++ index.js
@@ -5,15 +5,60 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'your github username?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'your email address?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'your project description?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'license type?',
+        choices: ["MIT", "None"]
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'what command to run install dependencies?',
+        default: 'npm i'
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'what command to run test?',
+        default: 'npm test'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'what info does user require?'
+    },
 
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then((response) => {
+        console.log(`Generating README...`);
+        writeToFile('README.md', generateMarkdown({...response}))
+        
+    }) 
 
 }
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,20 @@
 // function to generate markdown for README
+
+function renderLicenseBadge(license) {
+  if(license !== "None") {
+    return `![Github license](https://img.shields.io/badge/liences-${liences}-blue.svg)`
+  }
+  return ''
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
 
-`;
-}
+  ### Description
+  'hello'
+
+`; 
+} 
 
 module.exports = generateMarkdown;
